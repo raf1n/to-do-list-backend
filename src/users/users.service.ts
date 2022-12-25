e335076c34ca0e2280bd56e1f7d7e56b0a69fd2b
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2022-12-25 20:13:27 +0600

    user-schema-and-post-endpoint-added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,16 @@
+import { User ,UserDocument} from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  async create(createUserDto: CreateUserDto):Promise<UserDocument> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
   }
 
   findAll() {
