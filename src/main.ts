e3eb2414e7f712f90162e0cbcdea9af26b2a20bd
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-03-12 01:14:54 +0600

    l

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
+  const port = process.env.PORT || 5000;
 
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(5000);
+  await app.listen(port);
 }
 bootstrap();
